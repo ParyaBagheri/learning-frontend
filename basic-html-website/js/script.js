bd823e6ca5995f2fa2e6a4c5131d948f38e88874
aylinnof <aylin.noferesti@gmail.com>
2026-08-16 15:16:52 +0330

    update CSS and JavaScript

--- basic-html-website/js/script.js
+++ basic-html-website/js/script.js
@@ -16,3 +16,16 @@ for (const skill of skills) {
     skillsList.appendChild(listItem);
 
 }
+
+const toggleButton = document.getElementById("toggle-skills-button");
+    toggleButton.addEventListener("click", function () {
+
+        if (skillsList.style.display === "none") {
+            skillsList.style.display = "block"
+            toggleButton.textContent = "Hide Skills"
+        } else {
+            skillsList.style.display = "none";
+            toggleButton.textContent = "Show Skills"
+        }    
+
+    });
